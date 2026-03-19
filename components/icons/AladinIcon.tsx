d0012ab3cd2e5216ce3a6b6aa99ca1dd51621a46
T-STUDIO <tstudio.astro@gmail.com>
2026-03-19 21:15:29 +0900

    Add files via upload

--- components/icons/AladinIcon.tsx
+++ components/icons/AladinIcon.tsx
@@ -1,1 +1,10 @@
 
+import React from 'react';
+export const AladinIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z"></path>
+    <path d="M12 12l4.95-4.95"></path>
+    <path d="M12 12V2"></path>
+    <path d="m4.91 4.91 9.18 9.18"></path>
+  </svg>
+);

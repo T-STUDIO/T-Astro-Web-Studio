3e9ef13b1c2799d4a6e07f12f01e3f774dfff29a
T-STUDIO <tstudio.astro@gmail.com>
2026-06-11 21:37:54 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    const isGitHubPages = process.env.GITHUB_ACTIONS === 'true' || process.env.NODE_ENV === 'production';
+    const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 
     return {
       base: isGitHubPages ? '/T-Astro-Web-Studio/' : './',

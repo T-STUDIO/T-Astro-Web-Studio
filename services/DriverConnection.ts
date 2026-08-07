939f01bf08aeda020390d7c30053361f49bd71be
T-Studio <tstudio.astro@gmail.com>
2026-08-08 03:20:16 +0900

    Add files via upload

--- services/DriverConnection.ts
+++ services/DriverConnection.ts
@@ -115,6 +115,13 @@ export const getActiveMount = () => activeMountDevice;
 export const getActiveCamera = () => activeCameraDevice;
 export const setActiveCamera = (devName: string) => {
     activeCameraDevice = devName;
+    if (devName && discoveredIndiDevices.has(devName)) {
+        if (mainChannelBlobsDisabled) {
+            sendRaw(`<enableBLOB device='${devName}'>Never</enableBLOB>`);
+        } else {
+            sendRaw(`<enableBLOB device='${devName}'>Also</enableBLOB>`);
+        }
+    }
     scheduleUpdate();
 };
 export const getActiveFocuser = () => activeFocuserDevice;
